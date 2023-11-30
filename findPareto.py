cdd28d99cc867512233f80571e0473568aa3e62a
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-11-30 15:09:45 +0100

    kokolores

--- findPareto.py
+++ findPareto.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from TwoPlayerClass import * 
 
 # Generate data for the plots
 x = np.linspace(0, 1, 100)
@@ -47,9 +48,69 @@ Z2 = Y/(1+(X+Y)**2)  # Quadratic colormap 1
 #plt.show()
 
 
+def test(a,b,c): 
+    return 0
+
+
+def testPareto(matrix): 
+
+
+    paretoMatrix = np.ones((len(matrix), len(matrix)))
+
+    for i in range(len(matrix)): 
+        for j in range(len(matrix)): 
+
+            if i == 0 and j == 1: 
+                print('test')
+
+            payOffOne, payOffTwo = matrix[i, j]
+
+            for k in range(len(matrix)):
+                for l in range(len(matrix)): 
+                    if k == i or l == j: 
+                        continue
+
+                    otherPayOffOne, otherPayOffTwo = matrix[k, l]
+
+                    if otherPayOffOne >= payOffOne and otherPayOffTwo >= payOffTwo: 
+                        paretoMatrix[i, j] = False
+                        # print(otherPayOffOne , 'bigger',  payOffOne, 'and',  otherPayOffTwo, 'beq' , otherPayOffOne)
+
+                        continue
+            
+
+    
+    return paretoMatrix
+
+
+game = tp_game([1,1], [1,1], 20, test)
+
+matrix = game.uti_matrix()
+
+# matrix = np.array([[(3,3), (5,1)], [(1,5), (1,1)]])
+
+testMatrix = testPareto(matrix)
+
+print(matrix)
+
+print(testMatrix)
+
+
+
+
+
+
+
+
+
+
+
+
+# OTHER APPROACHs
+
 def FindPareto(x): 
 
-    step = 0.01
+    step = 0.1
 
     arrayOfInterest = np.zeros((int(2/(step)), len(x)))
 
@@ -106,17 +167,70 @@ def FindPareto(x):
         # plt.plot(xTemp, xArray)
         # plt.scatter(xTemp[tempMaxIndex], xArray[tempMaxIndex])
 
-    return xList, yList
+    xList2 = []
+    yList2 = []
+
+    for i in range(len(test)):
+        j = test[i]
+
+        
+
+        y = x + j
+
+        a = np.clip(y, 0, 1)
+
+        indices = np.where(y == a)
+
+        xTemp = x[indices]
+
+        yTemp = y[indices]
+
+        print(np.shape(a))
+
+        # tempArray = x/(1+(a+x)**2)
+        xArray = yTemp/(1+(xTemp+j+xTemp)**2)
+        xTest = y/(1+(y+x)**2)
+
+        # arrayOfInterest[i, :] = x/(1+(x+j+x)**2)
+
+        # tempMaxIndex = np.argmax(arrayOfInterest[i])
+
+        tempMax = np.max(xArray)
+
+        # tempMaxIndex = np.where(xTest == tempMax)
+        tempMaxIndex = np.where(xArray == tempMax)
+        # xMaxList.append(tempMax)
+
+        tempMaxIndexOld = np.where(xTest == tempMax)
+
+        xList2.append(x[tempMaxIndexOld])
+        yList2.append(y[tempMaxIndexOld])
+
+        # plt.plot(x, arrayOfInterest[i])
+        # plt.plot(x[tempMaxIndex], y[tempMaxIndex])
+        # plt.plot(x, xTest)
+
+        # show funny pattern
+        # plt.plot(xTemp, xArray)
+        # plt.scatter(xTemp[tempMaxIndex], xArray[tempMaxIndex])
+
+
+
+
+
+
+    return xList, yList, xList2, yList2
 
 
 
         
 
-X, Y = FindPareto(x)
+X, Y, X2, Y2 = FindPareto(x)
 
 # print(X, Y)
 
 plt.plot(X, Y, label ='Pareto optima developement')
+plt.plot(X2, Y2)
 plt.xlim(0,1)
 plt.ylim(0,1)
 plt.title('Pareto Optima for fishermen')
