5de41a46c2c75f5c3266e859b6c35fddb076fc74
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-12-10 19:38:02 +0100

    update

--- DuringPresentation.py
+++ DuringPresentation.py
@@ -12,30 +12,36 @@ def ShowHeatMap(taxFunction, impactList, efficiencyList):
 
 def sinus(action, eff, imp): 
 
-    return np.sin(10*action * eff / imp)
+    return -np.sin(10*action * eff / imp)
+
+def quadratic(action, eff, imp): 
+    '''
+    interesting, for 0.5 * ..., we have two pareto optima 
+    '''
+    return -(0.5 * action * imp / eff)**2
+
 
 def simple(action, eff, imp): 
 
-    return action * eff / imp
+    return -0.1 * action * eff / imp
+
+def simple2(action, eff, imp): 
+    '''
+    for 0.18*... we get two NE!!
+    '''
+    return -0.18 * action * eff / imp
 
 def cutOff(action, eff, imp):
 
-    return -10*np.heaviside(action - 2 * eff / imp, 1)
+    return -10*np.heaviside(action - 0.5 * eff / (2*imp), 1)
 
-def cutOffImp(action, eff, imp): 
 
-    return -10*np.heaviside(action - 0.2 * eff / imp, 1)
 
 def ExpFct(action, eff, imp):
-
-    return np.exp(action)
-
-
-
-
-
-impactList = [random.uniform(0.8, 1), random.uniform(0.8, 1)]
-efficiencyList = [random.uniform(0.8, 1),random.uniform(0.8, 1)]
+    '''
+    for different impact, well visible! 
+    '''
+    return -0.05 * np.exp(action * eff / imp)
 
 
 # ShowHeatMap(cutOffImp, impactList, efficiencyList)
@@ -43,10 +49,31 @@ efficiencyList = [random.uniform(0.8, 1),random.uniform(0.8, 1)]
 def no_tax(action,eff,imps):
     return 0
 
+# indep of eff and imp
 def cutoff(action,eff,imps):
     return -10*np.heaviside(action - 0.5,1)
 
 
-game1 = tp_game([1,1],[1,1],20,cutoff)
+# same eff and imp
+game1 = tp_game([1,1],[1,0.5],30,sinus)
 game1.ShowHeatMap()
+
+# # diff efficiency
+# game2 = tp_game([1,0.5],[1,1],30,cutOff)
+# game2.ShowHeatMap()
+
+# # diff impact 
+# game3 = tp_game([1,1],[1,0.5],30,cutOff)
+# game3.ShowHeatMap()
+
+
+# vary functions: 
+# game = tp_game([1,1],[1,0.5],20,no_tax)
+# game.ShowHeatMap()
+
+# game = tp_game([1,1],[1,0.5],20,simple)
+# game.ShowHeatMap()
+
+# game = tp_game([1,1],[1,0.5],20,simple2)
+# game.ShowHeatMap()
 
